f108632d05bfc497e538f014a2bc37685662055e
jyrnan <yoeking@gmail.com>
2022-08-05 15:04:07 +0800

    Initial Commit

---
+++ TestMultiLinkSDK/TestMultiLinkSDK.h
@@ -0,0 +1,18 @@
+//
+//  TestMultiLinkSDK.h
+//  TestMultiLinkSDK
+//
+//  Created by Yong Jin on 2022/8/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestMultiLinkSDK.
+FOUNDATION_EXPORT double TestMultiLinkSDKVersionNumber;
+
+//! Project version string for TestMultiLinkSDK.
+FOUNDATION_EXPORT const unsigned char TestMultiLinkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestMultiLinkSDK/PublicHeader.h>
+
+
